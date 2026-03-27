72368f5938209172906cbc20edc595feb19b7a0b
Shaw <campbellshaw8@gmail.com>
2026-03-27 14:53:30 +0200

    Added IScheduler Interface

---
+++ include/IScheduler.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+#include "Task.h"
+#include "ScheduleResult.h"
+
+class IScheduler {
+public:
+    // Each concreate scheduler will make it's own internal copy if it needs to sort
+    virtual ScheduleResult schedule(const std::vector<Task>& tasks, int timeLimitSlots) = 0;
+    virtual ~IScheduler() = default;
+};
+
