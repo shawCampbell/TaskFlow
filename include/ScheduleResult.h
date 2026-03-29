6e1c19fc4e4b1c85f686c34e11929a08f569d717
Shaw <campbellshaw8@gmail.com>
2026-03-29 19:40:57 +0200

    Update ScheduleResult to use CompletedTask for scheduled tasks

--- include/ScheduleResult.h
+++ include/ScheduleResult.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <vector>
 #include "Task.h"
+#include "CompletedTask.h"
 
 struct ScheduleResult {
-    std::vector<Task> scheduledTasks;
+    std::vector<CompletedTask> scheduledTasks;
     std::vector<Task> deferredTasks;
     int               totalTimeUsed;
 };
